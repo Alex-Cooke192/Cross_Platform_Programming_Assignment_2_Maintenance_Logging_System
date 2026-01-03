293a631cf75afeeba6c59fd8e3542c04f0d1cfcc
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-03 20:08:19 +0000

    modified app to listen to theme changes

--- lib/app.dart
+++ lib/app.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
-import 'core/theme/app_theme.dart';
+import 'package:maintenance_logging_system/core/theme/app_theme.dart';
+import 'package:maintenance_logging_system/core/theme/theme_controller.dart';
+import 'package:maintenance_logging_system/screens/login_screen.dart';
+
+final ThemeController themeController = ThemeController();
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RampCheck',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      home: const AppRoot(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeController.themeMode,
+      builder: (context, themeMode, _) {
+        return MaterialApp(
+          title: 'RampCheck',
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: themeMode,
+          debugShowCheckedModeBanner: false,
+          home: const AppRoot(),
+        );
+      },
     );
   }
 }
 
-/// App-level decision widget
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TEMP: replace later with login/sync logic
-    return const Scaffold(
-      body: Center(
-        child: Text('RampCheck Home'),
-      ),
-    );
+    return const LoginScreen();
   }
 }
 
