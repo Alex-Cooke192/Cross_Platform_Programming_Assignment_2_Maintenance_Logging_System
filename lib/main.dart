d151e902d7cb402d79f86a51f1feb5ff90e0c038
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-06 22:03:11 +0000

    now creates database, dao objects and repositories

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:maintenance_logging_system/data/local/app_database.dart';
+import 'package:maintenance_logging_system/data/local/daos/inspection_dao.dart';
+import 'package:maintenance_logging_system/data/local/daos/outbox_dao.dart';
+import 'package:maintenance_logging_system/data/local/daos/task_dao.dart';
+import 'package:maintenance_logging_system/data/repositories/inspection_repository.dart';
 import 'app.dart';
 
 void main() {
+  final db = AppDatabase()
+  final inspectionDao = InspectionDao(db); 
+  final taskDao = TaskDao(db); 
+  final outboxDao = OutboxDao(db); 
+
+  final inspectionRepo = InspectionRepository(
+    inspectionDao: inspectionDao, 
+    taskDao: taskDao, 
+    outboxDao: outboxDao);
+  // final taskRepo = TaskRepo(); 
+
   runApp(const App());
 }
