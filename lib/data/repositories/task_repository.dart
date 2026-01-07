23bba48cb009429dc5416c171243db3a41f9d7fc
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-07 00:01:37 +0000

    added comments

--- lib/data/repositories/task_repository.dart
+++ lib/data/repositories/task_repository.dart
@@ -14,6 +14,7 @@ class TaskRepository {
     return _taskDao.watchForInspection(inspectionId);
   }
 
+  // Reads task by id
   Stream<Task?> watchById(String taskId) {
     return _taskDao.watchById(taskId);
   }
